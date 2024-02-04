6380308dc9bf4d30fa33a11d0901d5ab73c2c103
chelseaqs26 <chelseaquaies@gmail.com>
2024-02-04 12:13:11 -0500

    Adding Temperature

--- src/app.js
+++ src/app.js
@@ -1,8 +1,21 @@
+function updateWeatherData(response) {
+  let temperatureElement = document.querySelector("#temperature");
+
+  temperatureElement.innerHTML = Math.round(temperature);
+}
+
+function searchCity(city) {
+  let apiKey = "ee21f04bf3f2ad6e420ef9to7c4ad1a4";
+  let apiURL = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric"`;
+  axios.get(apiURL).then(updateWeatherData);
+}
+
 function searchSubmit(event) {
   event.preventDefault();
   let searchInput = document.querySelector("#search-form-input");
   let cityElement = document.querySelector("#city");
   cityElement.innerHTML = searchInput.value;
+  searchCity(searchInput.value);
 }
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", searchSubmit);
